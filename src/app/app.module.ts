54e49526331c79ef00520ee8b3762d39d40c2c6f
Santhosh Kumar Bala Krishnan <santhosh.balakrishnan@okta.com>
2018-06-13 15:43:46 -0700

    Added toggle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { UiSwitchModule } from 'ngx-ui-switch';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UiSwitchModule.forRoot({
+      size: 'small',
+      color: '#27C78B',
+      switchColor: '#282A35',
+      defaultBgColor: '#E02D67',
+      defaultBoColor: 'transparent'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
